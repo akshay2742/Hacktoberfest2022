ac3d9726c06c0bce11c1da4f6311d4789c997964
Josua Sinaga <josua123690707@gmail.com>
2022-10-11 04:17:54 +0700

    Update MergeTwoSortedArray_Josua.kt

--- MergeTwoSortedArray_Josua.kt
+++ MergeTwoSortedArray_Josua.kt
@@ -1,7 +1,5 @@
-fun main() {
-    val arr1 = arrayOf(3,5,6,9,23,45,12,2)
-    val arr2 = arrayOf(1,11,21,73,26,27)
-    val result = (arr1 + arr2).sorted()
+val arr1 = arrayOf(3,5,6,9,23,45,12,2)
+val arr2 = arrayOf(1,11,21,73,26,27)
+val result = (arr1 + arr2).sorted()
     
-    print(result)
-}
+print(result)
