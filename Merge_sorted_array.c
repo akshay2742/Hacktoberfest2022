3a91e9fbd14f27d136a25032e2d567c973e1a5a2
SHIVANI GUPTA <100036984+Shivu-2000@users.noreply.github.com>
2022-10-12 23:14:23 +0530

    IN place merging of two sorted arrays in C

---
+++ Merge_sorted_array.c
@@ -0,0 +1,41 @@
+//Inplace merging of two sorted arrays
+#include<stdio.h>
+int main()
+{
+    int a1,m,n,b1;
+    scanf("%d",&a1);
+    scanf("%d",&b1);
+    int a[a1];
+    int b[b1];
+    m=a1;
+    n=b1;
+    int i,j=0,k;
+    for(i=0;i<a1;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<b1;i++)
+    {
+        scanf("%d",&b[i]);
+    }
+    for(i=m;i<a1;i++)
+    {
+        a[i]=b[j];
+        j++;
+    }
+    for(i=1;i<a1;i++)
+    {
+        k=a[i];
+        j=i-1;
+        while(j>=0 && a[j]>k)
+        {
+             a[j + 1] = a[j];
+             j = j - 1;
+        }
+        a[j + 1] = k;
+    }
+    for(i=0;i<m+n;i++){
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
